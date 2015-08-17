23e8c221f1b1be0b1275ac48aeaad1316ee0fdc3
gonzalopelos <gonzalo.pelos@gmail.com>
2015-08-17 10:26:28 -0300

    Firmware and custom demos
    Código del firmware y demos para interactuar con la placa. PIC18F45K50.

---
+++ pocs/Custom_Demos/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Custom_Demos.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Custom_Demos.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=customdemos.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/customdemos.tar
