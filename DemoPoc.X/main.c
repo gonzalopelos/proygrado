0468caf8a60e1aacc1cffc0574baa56508b875de
GonzaloP <gonzalo.pelos@gmail.com>
2015-09-16 01:02:27 -0300

    Modificaciones realizadas para leer un boton y variar la frecuencia de parpadeo de un led

--- DemoPoc.X/main.c
+++ DemoPoc.X/main.c
@@ -8,26 +8,48 @@
 #include "configs.h"
 #include <xc.h>
 
+unsigned long _maxTimeOut;
 
-void sleep(void)
+void sleep(unsigned long timeout)
 {
-    int clock = 0;
-    int timeout = 1000000;
+    unsigned long clock = 0;
+    
+    
     while(clock < timeout)
     {
         clock = clock + 1;
     }
 }
 
+void checkButtonStatus(void) {
+    if(PORTAbits.RA2) {
+        _maxTimeOut = _maxTimeOut / 2; 
+    }
+    
+    if (_maxTimeOut == 1)
+       _maxTimeOut = 131072;
+}
+
+void initialize(void) {
+    //TRISD = 0; // se configuran los pines asociados al puerto D para salida
+    TRISA = 0; // se configuran todos los pines asociados al puerto C para salida
+    TRISAbits.RA2 = 1; // se configura el pin asociado al bit 0 del puerto A para lectura de entrada anal[ogica]
+    ANSELAbits.ANSA2= 0;               // set pin as digital
+    _maxTimeOut = 131072;
+}
 
 void main(void) {
-    TRISD = 0;
+    initialize();
     while (1) {
-        LATDbits.LATD0 = 1;
-        sleep();
-        LATDbits.LATD0 = 0;
-        sleep();
+        LATAbits.LATA0 = 1;
+        sleep(_maxTimeOut);
+        LATAbits.LATA0 = 0;
+        sleep(_maxTimeOut);
+        checkButtonStatus();
     }
 
 }
+
+
+
 
