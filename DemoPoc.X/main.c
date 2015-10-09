227b47fdd4c7874b717cd0d1017d8af297c9eaa2
GonzaloP <gonzalo.pelos@gmail.com>
2015-10-08 23:22:19 -0300

    Codigo para leer una entrada analogica en el pin RA3 y generar una salida digital en el pin RB2. Acceder a los links presentes en el metodo init para entender las configuraciones

--- DemoPoc.X/main.c
+++ DemoPoc.X/main.c
@@ -9,6 +9,7 @@
 #include <xc.h>
 
 unsigned long _maxTimeOut;
+int _analogValue;
 
 void sleep(unsigned long timeout)
 {
@@ -31,19 +32,46 @@ void checkButtonStatus(void) {
 }
 
 void initialize(void) {
-    //TRISD = 0; // se configuran los pines asociados al puerto D para salida
-    TRISA = 0; // se configuran todos los pines asociados al puerto C para salida
+    //ver p[agina 288 de la cartilla https://drive.google.com/open?id=0B5b9cF_5KTdQd3g5VUFWa3VheFU
+    //ver http://www.learningaboutelectronics.com/Articles/PIC-analog-to-digital-conversion-ADC.php
+    
+    TRISA = 0; 
     TRISB = 0;
-    TRISAbits.RA2 = 1; // se configura el pin asociado al bit 0 del puerto A para lectura de entrada anal[ogica]
-    //ANSELAbits.ANSA2= 0;               // set pin as digital
+    TRISAbits.RA3 = 1; 
+    ANSELAbits.ANSA3=1;
+    
+    ADCON2bits.ADFM=1;
+    ADCON2bits.ACQT=1;
+    ADCON2bits.ADCS=2;
+    
+    ADCON1bits.PVCFG0 =0;
+    ADCON1bits.NVCFG1 =0;
+    
+    ADCON0bits.CHS = 3;
+    
+    ADCON0bits.ADON = 1;
+    
     _maxTimeOut = 131072;
-    PORTA = 0;
 }
 
 void main(void) {
     initialize();
+    LATBbits.LATB2 = 0;
     while (1) {
-        LATBbits.LATB2 = PORTAbits.RA2;
+
+        ADCON0bits.GODONE=1;
+        
+        while(ADCON0bits.GODONE==1);
+        
+        _analogValue = ADRESL + (ADRESH *256);
+        
+        if (_analogValue > 700){
+            LATBbits.LATB2 = 1;
+        }
+        else{
+            LATBbits.LATB2 = 0;
+        }
+        
     }
 
 }
