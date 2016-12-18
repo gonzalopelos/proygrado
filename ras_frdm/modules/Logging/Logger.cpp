5132007a81fe021db61caee718dd2826a3061fa2
Gonzalo Pelos <gonzalo.pelos@gmail.com>
2016-12-18 18:59:06 -0300

    Modificaciones en Logger para registrar un mensaje ALIVE cuando no hay nada para logear.

--- ras_frdm/modules/Logging/Logger.cpp
+++ ras_frdm/modules/Logging/Logger.cpp
@@ -14,10 +14,9 @@
 #define MBED_DEV_MASK     		"255.255.255.0"
 #define MBED_DEV_GW       		"0.0.0.0"
 #define ECHO_SERVER_PORT   		5000
-#define ALIVE_MESSAGE			"ALIVE\n"
 
 Thread _socket_server_thread;
-Mutex _buffer_message_mutex;
+//Mutex _buffer_message_mutex;
 
 const int _MESSAGE_BUFFER_CAPACITY = 128;
 
@@ -33,12 +32,13 @@ typedef struct {
 } logger_message_t;
 
 
-Queue<logger_message_t, _MESSAGE_BUFFER_CAPACITY> _messages;
-
+Mail<logger_message_t, _MESSAGE_BUFFER_CAPACITY> _messages;
+int _allive_message_counter;
 
 void socket_server_thread_func();
 logger_message_t*  get_message_to_send();
-void remove_sent_message();
+
+int get_allive_message(char * message);
 
 Logger::Logger() {
 	init();
@@ -46,15 +46,19 @@ Logger::Logger() {
 
 int Logger::write_trace(char* data) {
 	int result = 0;
-	logger_message_t* message_data = (logger_message_t*)malloc(sizeof(logger_message_t));
-	bzero(message_data->data, sizeof(message_data->data));
-	result = sprintf(message_data->data, data);
-	message_data->size = result;
+//	_buffer_message_mutex.lock();
 
-	_buffer_message_mutex.lock();
-	_messages.put(message_data);
+	logger_message_t* message_data = _messages.alloc();
+	if(message_data != NULL){
+		bzero(message_data->data, sizeof(message_data->data));
+		result = sprintf(message_data->data, data);
+		message_data->size = result;
+		_messages.put(message_data);
 
-	_buffer_message_mutex.unlock();
+	}
+
+//	_buffer_message_mutex.unlock();
+
 	return result;
 }
 
@@ -65,6 +69,7 @@ void Logger::init() {
 	_socket_server.bind(ECHO_SERVER_PORT);
 	_socket_server.listen();
 	_socket_server_thread.start(&socket_server_thread_func);
+	_allive_message_counter = 0;
 }
 
 Logger::~Logger() {
@@ -78,12 +83,12 @@ Logger * Logger::get_instance() {
 	return _logger_instance;
 }
 
-
 void socket_server_thread_func() {
 
 	logger_message_t* message;
-
-	while (true) {
+	char allive_message[100];
+	int allive_message_size;
+	while (1) {
 		_socket_server.accept(_socket_client);
 		_socket_client.set_blocking(false, 1500); // Timeout after (1.5)s
 		while(_socket_client.is_connected()){
@@ -91,12 +96,13 @@ void socket_server_thread_func() {
 			if( message != NULL){
 				if(message->size > 0){
 					_socket_client.send_all(message->data, message->size);
-					free(message);
 				} else {
 					_socket_client.send_all("message size 0\n", 14);
 				}
+				_messages.free(message);
 			}else {
-				_socket_client.send_all(ALIVE_MESSAGE, 5);
+				allive_message_size = get_allive_message(allive_message);
+				_socket_client.send_all(allive_message, allive_message_size);
 			}
 			wait(1);
 		}
@@ -106,13 +112,22 @@ void socket_server_thread_func() {
 
 logger_message_t* get_message_to_send() {
 	logger_message_t* message = NULL;
-	_buffer_message_mutex.lock();
+//	_buffer_message_mutex.lock();
+
 	osEvent evt = _messages.get(500);
-	if (evt.status == osEventMessage) {
+	if (evt.status == osEventMail) {
 		message = (logger_message_t*)evt.value.p;
 	}
-	_buffer_message_mutex.unlock();
+
+//	_buffer_message_mutex.unlock();
 
 	return message;
 }
 
+int get_allive_message(char * allive_message) {
+	int res = 0;
+	bzero(allive_message, sizeof(allive_message));
+	res = sprintf(allive_message, "ALLIVE: %d\n", _allive_message_counter);
+	_allive_message_counter = (_allive_message_counter + 1) % 10000;
+	return res;
+}
