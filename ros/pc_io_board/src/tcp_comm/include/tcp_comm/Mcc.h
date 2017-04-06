bdcd9817a70510213b94cbac3b4b2cf79165bbb2
Gonzalo Pelos <gonzalo.pelos@gmail.com>
2017-04-06 01:02:42 -0300

    ROS tcp_comm + Se agrega procesamiento de mensajes desde la ioboard + Se agerga publisher para mensajes que llegan desde la ioboard + Se agrega procesamiento de mensajes que llegan de nodos ros para la ioboard + Se agrega subscriber para mensajes que publican nodos ros para ser enviados a la ioboard.

--- ros/pc_io_board/src/tcp_comm/include/tcp_comm/Mcc.h
+++ ros/pc_io_board/src/tcp_comm/include/tcp_comm/Mcc.h
@@ -16,7 +16,7 @@
 
 
 typedef void (*PollCallback) ();
-typedef int (*OpcodeCallback) (unsigned int pid, unsigned int opcode);
+typedef int (*OpcodeCallback) (unsigned int pid, unsigned int opcode, char* bc_params);
 typedef OpcodeCallback* OpcodeCallbacks;
 
 struct OpcodesCallbackReg {
@@ -32,9 +32,9 @@ class Mcc {
     OpcodesCallbackReg opcode_callbacks[MAX_PIDS];
     uint8_t opcode_callbacks_count[MAX_PIDS]; //FIXME merge with previous as an array of structs.
     int n_opcode_callbacks;
-    void process_incomming();
     void send_parse_error_message();
     void send_execution_error_message(int tpid, int opcode, int errcode);
+    char* create_mcc_params_from_incomming_data();
 public:
     Mcc();
     virtual ~Mcc();
@@ -42,10 +42,12 @@ public:
     int incomming_params_n[MAX_PARAMS];
     int incomming_params_count;
     EmBencode encoder;
+    void process_incomming(char ch);
     int register_poll_callback(PollCallback cb);
     void unregister_poll_callback(int);
     int register_opcode_callbacks(OpcodeCallback* opcode_callbacks, uint8_t opcodes_count);
     void send_message(int spid, uint8_t opcode, char const *data, uint8_t data_length);
-    void tick();
+    // void tick();
+    char * create_mcc_message(int spid, uint8_t opcode, char const * params_bencoded);
 };
 #endif //TCP_COMM_MCC_H
--- ros/pc_io_board/src/tcp_comm/src/Mcc.cpp
+++ ros/pc_io_board/src/tcp_comm/src/Mcc.cpp
@@ -15,47 +15,53 @@ char error_str[] = "ERROR";
 char parseerror_str[] = "PARSEERROR";
 char exeerror_str[] = "EXEERROR";
 
-
-enum ProtocolStates {
-    MESSAGE, TPID, OPCODE, DATA, DATA_ITEM, END, NONE
+enum ProtocolStates
+{
+    MESSAGE,
+    TPID,
+    OPCODE,
+    DATA,
+    DATA_ITEM,
+    END,
+    NONE
 };
 
 enum ProtocolStates protocol_state = MESSAGE;
 char embuf[255];
 char outbuf[255];
-EmBdecode decoder(embuf, sizeof embuf);
-Tcp_Controller * host;
+char mcc_message[255];
+bool mcc_message_ready;
+
+EmBdecode decoder(embuf, sizeof(embuf));
 uint8_t tpid;
 uint8_t opcode;
+
 pthread_mutex_t serial_mcc_frame_lock;
 
 /***********************
  * EmBencode Operations
  ***********************/
-void EmBencode::lock() {
+void EmBencode::lock()
+{
     pthread_mutex_lock(&serial_mcc_frame_lock);
     // serial_mcc_frame_lock.lock();
 }
 
-void EmBencode::unlock() {
+void EmBencode::unlock()
+{
     pthread_mutex_unlock(&serial_mcc_frame_lock);
     // serial_mcc_frame_lock.unlock();
 }
 
-void EmBencode::PushChar(char ch) {
-    ////////Serial.write(ch);
-    /*led4 = 1;
-    while (pc.writeable()==0) {}
-    pc.putc(ch);
-    led4 = 0;*/
-    //ToDo: send tcp message.
-    if(strlen(outbuf) < 255){
+void EmBencode::PushChar(char ch)
+{
+    if (strlen(outbuf) < 255)
+    {
         outbuf[strlen(outbuf)] = ch;
-        if(ch == '\n'){
-            printf("MCC :: Data sent: %s\n", outbuf);
-            host->send_all(outbuf, strlen(outbuf));
-            // 2017.02.28 AM - Cambio bzero por memset.
-            //bzero(outbuf, 255);
+        if (ch == '\n')
+        {
+            memset(mcc_message, 0, 255);
+            strcpy(mcc_message, outbuf);
             memset(outbuf, 0, 255);
         }
     }
@@ -63,170 +69,191 @@ void EmBencode::PushChar(char ch) {
 
 /**************************/
 
-Mcc::Mcc() {
+Mcc::Mcc()
+{
     incomming_params_count = 0;
     n_poll_callbacks = 0;
     n_opcode_callbacks = 0;
-    // 2017.02.28 AM - Cambio bzero por memset.
-    //bzero(outbuf, 255);
     memset(outbuf, 0, 255);
-    host = Tcp_Controller::get_instance();
 }
 
-Mcc::~Mcc() {
+Mcc::~Mcc()
+{
     // TODO Auto-generated destructor stub
 }
 
-void Mcc::process_incomming() {
-    //ToDo: Get data from TCP and process by char
-    char data[256];
-    char ch;
-    int data_length = 0;
+void Mcc::process_incomming(char ch)
+{
+    if (ch == '\n')
+    {
+        decoder.reset();
+        tpid = opcode = -1;
+        incomming_params_count = 0;
+        return;
+    }
 
-    data_length = host->receive(data, 1);
-    if(data_length > 0) {
-        ch = data[0];
-        for(int data_index =0;data_index<data_length;data_index++) {
-            if (data[0]=='\n') {
-                decoder.reset();
-                tpid = opcode = -1;
-                incomming_params_count = 0;
-                return;
+    uint8_t bytes = decoder.process(ch);
+    if (bytes > 0)
+    {
+        protocol_state = MESSAGE;
+        for (;;)
+        {
+            uint8_t token = decoder.nextToken();
+            if (token == EmBdecode::T_END)
+            {
+                //decoder.reset();
+                break;
             }
 
-            uint8_t bytes = decoder.process(ch);
-            if (bytes > 0) {
-                protocol_state = MESSAGE;
-                for (;;) {
-                    uint8_t token = decoder.nextToken();
-                    if (token == EmBdecode::T_END) {
-                        //decoder.reset();
-                        break;
-                    }
-
-                    switch (protocol_state) {
-                        case MESSAGE:
-                            if (token == EmBdecode::T_LIST) {
-                                //pc.putc('A');
-                                protocol_state = TPID;
-                            } else {
-                                //pc.putc('X');
-                                send_parse_error_message();
-                                protocol_state = NONE;
-                            }
-                            break;
-                        case TPID:
-                            if (token == EmBdecode::T_NUMBER) {
-                                //pc.('B');
-                                tpid = decoder.asNumber();
-                                protocol_state = OPCODE;
-                            } else {
-                                //pc.putc('X');
-                                send_parse_error_message();
-                                protocol_state = NONE;
-                            }
-                            break;
-                        case OPCODE:
-                            if (token == EmBdecode::T_NUMBER) {
-                                //pc.putc('C');
-                                opcode = decoder.asNumber();
-                                protocol_state = DATA;
-                            } else {
-                                //pc.putc('X');
-                                send_parse_error_message();
-                                protocol_state = NONE;
-                            }
-                            break;
-                        case DATA:
-                            if (token == EmBdecode::T_LIST) {
-                                //pc.putc('I');
-                                protocol_state = DATA_ITEM;
-                                incomming_params_count = 0;
-                            } else {
-                                //pc.putc('X');
-                                send_parse_error_message();
-                                protocol_state = NONE;
-                            }
-                            break;
-                        case DATA_ITEM:
-                            if (token == EmBdecode::T_STRING) {
-                                //pc.putc('S');
-                                uint8_t data_length;
-                                incomming_params_s[incomming_params_count] = decoder.asString(&data_length);
-                                incomming_params_n[incomming_params_count] = data_length;
-                                incomming_params_count++;
-                                if (incomming_params_count==MAX_PARAMS) {
-                                    //pc.putc('X');
-                                    send_parse_error_message();
-                                    protocol_state = NONE;
-                                }
-                            } else if (token == EmBdecode::T_NUMBER) {
-                                //pc.putc('N');
-                                incomming_params_s[incomming_params_count] = NULL;
-                                incomming_params_n[incomming_params_count] = decoder.asNumber();
-                                incomming_params_count++;
-                                if (incomming_params_count==MAX_PARAMS) {
-                                    //pc.putc('Y');
-                                    send_parse_error_message();
-                                    protocol_state = NONE;
-                                }
-                            } else if (token == EmBdecode::T_POP) {
-                                //pc.putc('H');
-                                protocol_state = END;
-                            } else {
-                                send_parse_error_message();
-                                protocol_state = NONE;
-                            }
-                            break;
-                        case END:
-                            if (token == EmBdecode::T_POP) {
-                                if (tpid < MAX_PIDS
-                                    && tpid < Mcc::n_opcode_callbacks
-                                    && opcode < (Mcc::opcode_callbacks[tpid]).n_callbacks) {
-                                    //pc.putc('!');
-                                    int ret = Mcc::opcode_callbacks[tpid].callbacks[opcode](tpid, opcode);
-                                    if (ret!=1) {
-                                        send_execution_error_message(tpid, opcode, ret);
-                                        //protocol_state = NONE;
-                                    }
-                                }
-                                //decoder.reset();
-                                protocol_state = NONE;
-                                //pc.putc('E');
-                            } else {
-                                //pc.putc('W');
-                                send_parse_error_message();
-                                protocol_state = NONE;
-                            }
-                            break;
-                        case NONE:
-                            break;
+            switch (protocol_state)
+            {
+            case MESSAGE:
+                if (token == EmBdecode::T_LIST)
+                {
+                    //pc.putc('A');
+                    protocol_state = TPID;
+                }
+                else
+                {
+                    //pc.putc('X');
+                    send_parse_error_message();
+                    protocol_state = NONE;
+                }
+                break;
+            case TPID:
+                if (token == EmBdecode::T_NUMBER)
+                {
+                    //pc.('B');
+                    tpid = decoder.asNumber();
+                    protocol_state = OPCODE;
+                }
+                else
+                {
+                    //pc.putc('X');
+                    send_parse_error_message();
+                    protocol_state = NONE;
+                }
+                break;
+            case OPCODE:
+                if (token == EmBdecode::T_NUMBER)
+                {
+                    //pc.putc('C');
+                    opcode = decoder.asNumber();
+                    protocol_state = DATA;
+                }
+                else
+                {
+                    //pc.putc('X');
+                    send_parse_error_message();
+                    protocol_state = NONE;
+                }
+                break;
+            case DATA:
+                if (token == EmBdecode::T_LIST)
+                {
+                    //pc.putc('I');
+                    protocol_state = DATA_ITEM;
+                    incomming_params_count = 0;
+                }
+                else
+                {
+                    //pc.putc('X');
+                    send_parse_error_message();
+                    protocol_state = NONE;
+                }
+                break;
+            case DATA_ITEM:
+                if (token == EmBdecode::T_STRING)
+                {
+                    //pc.putc('S');
+                    uint8_t data_length;
+                    incomming_params_s[incomming_params_count] = decoder.asString(&data_length);
+                    incomming_params_n[incomming_params_count] = data_length;
+                    incomming_params_count++;
+                    if (incomming_params_count == MAX_PARAMS)
+                    {
+                        //pc.putc('X');
+                        send_parse_error_message();
+                        protocol_state = NONE;
                     }
                 }
-                tpid = opcode = -1;
-                incomming_params_count = 0;
-
-                decoder.reset();
+                else if (token == EmBdecode::T_NUMBER)
+                {
+                    //pc.putc('N');
+                    incomming_params_s[incomming_params_count] = NULL;
+                    incomming_params_n[incomming_params_count] = decoder.asNumber();
+                    incomming_params_count++;
+                    if (incomming_params_count == MAX_PARAMS)
+                    {
+                        //pc.putc('Y');
+                        send_parse_error_message();
+                        protocol_state = NONE;
+                    }
+                }
+                else if (token == EmBdecode::T_POP)
+                {
+                    //pc.putc('H');
+                    protocol_state = END;
+                }
+                else
+                {
+                    send_parse_error_message();
+                    protocol_state = NONE;
+                }
+                break;
+            case END:
+                if (token == EmBdecode::T_POP) {
+                    // if (tpid < MAX_PIDS && tpid < Mcc::n_opcode_callbacks && opcode < (Mcc::opcode_callbacks[tpid]).n_callbacks)
+                    // {
+                        //pc.putc('!');
+                        // int ret = Mcc::opcode_callbacks[tpid].callbacks[opcode](tpid, opcode);
+                        int ret = Mcc::opcode_callbacks[0].callbacks[0](tpid, opcode, create_mcc_params_from_incomming_data());
+                        if (ret != 1)
+                        {
+                            send_execution_error_message(tpid, opcode, ret);
+                            //protocol_state = NONE;
+                        }
+                    // }
+                    //decoder.reset();
+                    protocol_state = NONE;
+                    //pc.putc('E');
+                } else {
+                    //pc.putc('W');
+                    send_parse_error_message();
+                    protocol_state = NONE;
+                }
+                break;
+            case NONE:
+                break;
             }
         }
+        tpid = opcode = -1;
+        incomming_params_count = 0;
+
+        decoder.reset();
     }
 }
 
-void Mcc::send_parse_error_message() {
+void Mcc::send_parse_error_message()
+{
     encoder.startFrame();
     encoder.push(ADMIN_PID);
     encoder.push(OPCODE_REPORT);
     encoder.startList();
-    encoder.push(parseerror_str, sizeof(parseerror_str)-1);
+    encoder.push(parseerror_str, sizeof(parseerror_str) - 1);
 
     encoder.push(tpid);
     encoder.push(opcode);
     encoder.push(protocol_state);
 
-    for (int i=0;i<incomming_params_count;++i) {
-        if (incomming_params_s[i] != NULL) {
+    for (int i = 0; i < incomming_params_count; ++i)
+    {
+        if (incomming_params_s[i] != NULL)
+        {
             encoder.push(incomming_params_s[i], incomming_params_n[i]);
-        } else {
+        }
+        else
+        {
             encoder.push(incomming_params_n[i]);
         }
     }
@@ -235,19 +262,24 @@ void Mcc::send_parse_error_message() {
     encoder.endFrame();
 }
 
-void Mcc::send_execution_error_message(int tpid, int opcode, int errcode) {
+void Mcc::send_execution_error_message(int tpid, int opcode, int errcode)
+{
     encoder.startFrame();
     encoder.push(ADMIN_PID);
     encoder.push(OPCODE_REPORT);
     encoder.startList();
-    encoder.push(exeerror_str, sizeof(exeerror_str)-1);
+    encoder.push(exeerror_str, sizeof(exeerror_str) - 1);
     encoder.push(errcode);
     encoder.push(tpid);
     encoder.push(opcode);
-    for (int i=0;i<incomming_params_count;++i) {
-        if (incomming_params_s[i] != NULL) {
+    for (int i = 0; i < incomming_params_count; ++i)
+    {
+        if (incomming_params_s[i] != NULL)
+        {
             encoder.push(incomming_params_s[i], incomming_params_n[i]);
-        } else {
+        }
+        else
+        {
             encoder.push(incomming_params_n[i]);
         }
     }
@@ -255,10 +287,12 @@ void Mcc::send_execution_error_message(int tpid, int opcode, int errcode) {
     encoder.endFrame();
 }
 
-int Mcc::register_poll_callback(PollCallback cb) {
+int Mcc::register_poll_callback(PollCallback cb)
+{
     int result = 0;
 
-    if (Mcc::n_poll_callbacks >= MAX_POLL_CALLBACKS) {
+    if (Mcc::n_poll_callbacks >= MAX_POLL_CALLBACKS)
+    {
         return -1;
     }
     Mcc::poll_callbacks[Mcc::n_poll_callbacks++] = cb;
@@ -267,15 +301,19 @@ int Mcc::register_poll_callback(PollCallback cb) {
     return result;
 }
 
-void Mcc::unregister_poll_callback(int n) {
-    if (n<Mcc::n_poll_callbacks-1){
-        Mcc::poll_callbacks[n] = Mcc::poll_callbacks[Mcc::n_poll_callbacks-1];
+void Mcc::unregister_poll_callback(int n)
+{
+    if (n < Mcc::n_poll_callbacks - 1)
+    {
+        Mcc::poll_callbacks[n] = Mcc::poll_callbacks[Mcc::n_poll_callbacks - 1];
     }
 }
 
-int Mcc::register_opcode_callbacks(OpcodeCallback* opcode_callbacks, uint8_t opcodes_count) {
+int Mcc::register_opcode_callbacks(OpcodeCallback *opcode_callbacks, uint8_t opcodes_count)
+{
     int result = -1;
-    if(Mcc::n_opcode_callbacks < MAX_PIDS){
+    if (Mcc::n_opcode_callbacks < MAX_PIDS)
+    {
         uint8_t pid = Mcc::n_opcode_callbacks++;
         Mcc::opcode_callbacks[pid].callbacks = opcode_callbacks;
         Mcc::opcode_callbacks[pid].n_callbacks = opcodes_count;
@@ -284,21 +322,54 @@ int Mcc::register_opcode_callbacks(OpcodeCallback* opcode_callbacks, uint8_t opc
     return result;
 }
 
-void Mcc::send_message(int spid, uint8_t opcode, const char* data, uint8_t data_length) {
+void Mcc::send_message(int spid, uint8_t opcode, const char *data, uint8_t data_length)
+{
     encoder.startFrame();
     encoder.push(spid);
     encoder.push(opcode);
     encoder.startList();
-    if (data!=NULL) {
+    if (data != NULL)
+    {
         encoder.push(data, data_length);
     }
     encoder.endList();
     encoder.endFrame();
 }
 
-void Mcc::tick() {
-    Mcc::process_incomming();
-    for (int i=0; i<Mcc::n_poll_callbacks; ++i){
-        Mcc::poll_callbacks[i]();
+// void Mcc::tick() {
+//     Mcc::process_incomming();
+//     for (int i=0; i<Mcc::n_poll_callbacks; ++i){
+//         Mcc::poll_callbacks[i]();
+//     }
+// }
+
+char *Mcc::create_mcc_message(int spid, uint8_t opcode, const char *params_bencoded)
+{
+    int params_bencoded_length = -1;
+    encoder.startFrame();
+    memset(mcc_message, 0, sizeof(mcc_message));
+    encoder.push(spid);
+    encoder.push(opcode);
+    params_bencoded_length = encoder.append_data(params_bencoded);
+    if (params_bencoded_length > 0)
+    {
+        encoder.endFrame();
     }
+
+    return mcc_message;
+}
+
+char* Mcc::create_mcc_params_from_incomming_data() {
+  encoder.startFrame();
+  memset(mcc_message, 0, sizeof(mcc_message));
+    for (int i=0;i< incomming_params_count;++i) {
+        if (incomming_params_s[i] != NULL) {
+            encoder.push(incomming_params_s[i], incomming_params_n[i]);
+            // printf("Mcc :: incomming_params_s received: %s\n", incomming_params_s[i]);
+        } else {
+            encoder.push(incomming_params_n[i]);
+        }
+    }
+  encoder.endFrame();
+  return mcc_message;
 }
