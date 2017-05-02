26cd480c607eb2b0515f837c36656d3494a949ec
Gonzalo Pelos <gonzalo.pelos@gmail.com>
2017-05-01 21:03:41 -0300

    + Add Ultrasonic library

--- ras_frdm/main.cpp
+++ ras_frdm/main.cpp
@@ -16,100 +16,123 @@ DigitalOut ledGreen(LED_GREEN);
 
 int main()
 {
-	led 		= 1;
-	ledGreen	= 1;
+	led = 1;
+	ledGreen = 1;
 	Dm3 *dm3 = Dm3::Instance();
 	int power = 0;
 	int i2cerror = 0;
 	int chasis = 0;
 	int motor = 0;
-    while (true) {
-    	dm3->enable(1);
-    	chasis = 0;
-    	motor = 1;
-    	power = 0;
+	while (true) {
+		dm3->enable(1);
+		chasis = 0;
+		motor = 1;
+		power = 0;
 		i2cerror = dm3->motor_i2c(chasis*MOTORS_PER_CHASIS + motor, power);
 		wait(2.0f);
-    	power = 50;
-    	i2cerror = dm3->motor_i2c(chasis*MOTORS_PER_CHASIS + motor, power);
-    	wait(2.0f);
-    	power = 100;
-    	i2cerror = dm3->motor_i2c(chasis*MOTORS_PER_CHASIS + motor, power);
+		power = 50;
+		i2cerror = dm3->motor_i2c(chasis*MOTORS_PER_CHASIS + motor, power);
+		wait(2.0f);
+		power = 100;
+		i2cerror = dm3->motor_i2c(chasis*MOTORS_PER_CHASIS + motor, power);
 
-    	wait(2.0f);
-    }
+		wait(2.0f);
+	}
 }
 
 /*
-int main() {
-	PIDModule pid[1];
-	uint8_t count = 0;
-	float vel = 0;
-    while(1) {
-    	wait(0.1);
-    	count++;
+ int main() {
+ PIDModule pid[1];
+ uint8_t count = 0;
+ float vel = 0;
+ while(1) {
+ wait(0.1);
+ count++;
 
-    	vel = pid[1].compute(0, 10);
+ vel = pid[1].compute(0, 10);
 
-    	DAC_I2C.write(count);
-    }
-}
-*/
+ DAC_I2C.write(count);
+ }
+ }
+ */
 
 #endif
 
 #ifndef MAIN_AM
 
-
 #include "mbed.h"
 #include "rtos.h"
-#include "modules/Mcc/Mcc.h"
-#include "modules/Admin/Admin.h"
-#include "modules/Motor/MotorModule.h"
-
+#include <Ultrasonic/Ultrasonic.h>
+//#include "modules/Mcc/Mcc.h"
+//#include "modules/Admin/Admin.h"
+//#include "modules/Motor/MotorModule.h"
 
 //#include "hdlc/frdm_communication.h"
 //#include "modules/Logging/Logger.h"
 //#include "modules/Ethernet/Communication.h"
 Ticker ticker_msg_rate;
 DigitalOut led_green(LED_GREEN);
+DigitalOut led_red(LED_RED);
+DigitalOut led_blue(LED_BLUE);
 using namespace modules;
-Mcc mcc;
+//Mock mcc;
 void heartbeat_task() {
-    while (true) {
-    	led_green = !led_green;
-        Thread::wait(500);
-    }
+	while (true) {
+		led_green = !led_green;
+		Thread::wait(500);
+	}
 }
+
+//================================================
+// TEST ULTRASONIC VARIABLES
+
+DigitalOut trigger(PTC17);
+
+DigitalIn echo(PTC16);
+int distance = 0;
+int correction = 0;
+Timer sonar;
+
+
+void dist(int distance)
+{
+   //put code here to happen when the distance is changed
+	led_red = 0;
+	printf("Distance changed to %d\r\n", distance);
+}
+
+Ultrasonic mu(PTC17, PTC16, .1, 1, &dist);
+
+
+//================================================
 
 int main() {
 
-//	Thread _pc_thread;
-//	main_pc.baud(9600);
-//	main_pc.format();
-	//int connection_id = 0;
-	//connection_id = open_frdm_connection()
-	DigitalOut led_red(LED_RED);
-	DigitalOut led_blue(LED_BLUE);
 	led_red = 1;
 	led_blue = 1;
 	led_green = 1;
-	wait(2);
+
+
+//	wait(2);
 //	mcc.send_message(0,0,"test",4);
-	Admin admin_module;
-	MotorModule motorModule;
+	//Admin admin_module;
+	//MotorModule motorModule;
 
-	motorModule.init();
-	Thread heartbeat(heartbeat_task);
+	//motorModule.init();
+	//Thread heartbeat(heartbeat_task);
 
-	while(1){
-		mcc.tick();
+//==================================================
+// ULTRASONIC LOGIC
+	mu.startUpdates();//start mesuring the distance
+	while(1)
+	{
+//  	Do something else here
+		mu.checkDistance();     //call checkDistance() as much as possible, as this is where
+//		the class checks if dist needs to be called.
+
 	}
 
 }
 
-
 #endif
-
-
 
