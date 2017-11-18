7ecab3a2b1bbed5ea6331f686c621ab6f32bedd7
Gonzalo Pelos <gonzalo.pelos@gmail.com>
2017-11-17 22:26:48 -0300

    Avance en la documentación de implementación. Secciones WDT y Monitoreo de conexión.

--- ras_frdm/main.cpp
+++ ras_frdm/main.cpp
@@ -8,6 +8,7 @@
 #include "Watchdog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <mbed_config.h>
 
 using namespace modules;
 Watchdog wdt;
@@ -20,10 +21,16 @@ DigitalOut led_red(LED_RED);
 DigitalOut led_blue(LED_BLUE);
 
 void heartbeat_task() {
+	unsigned int otro_count = 0;
 	while (true) {
+		otro_count++;
 		wdt.kick();	// Refresh WDT
-		led_green = !led_green;
-		Thread::wait((wdt.getTimeOutValue() * 1000) / 2);	// Wait WDT (toVal / 2) expressed in milliseconds.
+		if(otro_count > 1005000){
+			led_green = !led_green;
+			otro_count = 0;
+		}
+
+//		Thread::wait((wdt.getTimeOutValue() * 1000) / 2);	// Wait WDT (toVal / 2) expressed in milliseconds.
 	}
 }
 
@@ -82,15 +89,33 @@ void heartbeat_task() {
 //}
 //================================================
 
+void test_rtos(){
+
+	unsigned long my_count = 0;
+	while(1){
+		my_count++;
+		if(my_count > 30000000){
+			led_red = !led_red;
+			my_count = 0;
+			Thread::wait(100);
+		}
+	}
+}
+
+
 int main() {
 	led_blue = 1;
 	led_green = 1;
 	led_red = 1;
 	// Levantar hilo con heartbeat, contiene el kick del WDT.
-	Thread heartbeat;
+	Thread heartbeat(osPriorityNormal);
 	heartbeat.start(&heartbeat_task);
+	Thread test_thread(osPriorityHigh);
+	printf("la prioridad establecida es: %i\n", test_thread.get_priority());
+	test_thread.start(&test_rtos);
 
 
+/*
 
 	Admin* admin_module = Admin::get_instance();
 	MotorModule* motorModule_instance = MotorModule::get_instance();
@@ -118,9 +143,16 @@ int main() {
 
 	//	Thread battery_report_task;
 //	battery_report_task.start(callback(&Dm3Module::battery_report_task, &dm3Module));
-
+*/
+	unsigned int count = 0;
 	while(1){
-		mcc.tick();
+		count++;
+		if(count > 1000000){
+			led_blue = !led_blue;
+			count = 0;
+		}
+
+		//mcc.tick();
 		//Thread::wait(100);
 	}
 }
